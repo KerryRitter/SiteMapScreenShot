54a4f303736edb0d005239a368088426b6d28206
KerryRitter <ritter@kerryritter.com>
2013-09-10 12:45:40 -0500

    Initial commit with Chromedriver and PhantomJS driver for x64

---
+++ readme.md
@@ -0,0 +1,3 @@
+#SiteMapScreenShot.py
+
+To use this script, you will need to have Selenium and PhantomJS installed. Then update the site_domain and xml_file_name values and run the script. The files will, by default, be saved in a screenshots/ folder relative to the script.
---
+++ sitemapscreenshot.py
@@ -0,0 +1,69 @@
+from selenium import webdriver
+from selenium.common.exceptions import TimeoutException, StaleElementReferenceException
+from selenium.webdriver.support.ui import WebDriverWait 
+from xml.dom import minidom
+import time, os, string, requests, re, sys
+
+site_domain = "http://www.yourdomain.com/"
+xml_file_name = "sitemap.xml"
+
+def _slugify(text, delim=u'-'):
+    _punct_re = re.compile(r'[\t !"#$%&\'()*\-/<=>?@\[\\\]^_`{|},.]+')
+    result = []
+    for word in _punct_re.split(text.lower()):
+        if word:
+            result.append(word)
+    return "-".join(result)
+ 
+class SiteCapture:
+	driver = ""
+	save_folder = ""
+	domain = ""
+	site_map = ""
+
+	def __init__(self, driver, domain, site_map, save_folder):
+		self.driver = driver
+		if (driver == "phantomjs"):
+			try:
+				self.driver = webdriver.PhantomJS('phantomjs')
+			except:
+				print("Could not load a driver")
+				sys.exit()
+		elif (driver == "chrome"):
+			try:
+				self.driver = webdriver.Chrome('drivers/chromedriver')
+			except:
+				print("Could not load a driver")
+				sys.exit()
+		elif (driver == "firefox"):
+			self.driver = webdriver.Firefox()
+
+		self.domain = domain
+		self.site_map = site_map
+		self.save_folder = save_folder
+
+	def take_screenshot(self, page_url):
+		path = os.path.abspath(self.save_folder)
+		if not os.path.exists(path):
+			os.makedirs(path)
+		file_name = _slugify(page_url.replace(self.domain, ""))
+		if (file_name == ""):
+			file_name = "home"
+		full_path = path + "\\" + file_name + ".png"
+		print("Saving " + page_url + " as " + full_path)
+		self.driver.get_screenshot_as_file(full_path)
+		return full_path
+
+	def run(self):
+		xml = requests.get(self.domain + self.site_map)
+		xmldoc = minidom.parseString(xml.content)
+		itemlist = xmldoc.getElementsByTagName('url')
+		print (len(itemlist))
+		for item in itemlist:
+			page_url = (item.getElementsByTagName('loc')[0].firstChild.data)
+			self.driver.get(page_url)
+			self.take_screenshot(page_url)
+
+
+capture = SiteCapture("phantomjs", site_domain, xml_file_name, "screenshots/")
+capture.run()
